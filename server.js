7e836e4a1814a38d0143e24c362ebb83162b4987
MarsGotBars <marcinm@live.nl>
2025-02-27 19:39:28 +0100

    small update in case we ever need it

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const processedPeople = persons.map((person) => {
     return {
       firstName: capitalizedParts[0],
       // Pak de eerste 2 letters van de achternaam
-      processedName: capitalizedParts[1].slice(0, 2),
+      lastName: capitalizedParts[1],
       fullName: capitalizedParts[0] + capitalizedParts[1].slice(0, 2)
     };
   } catch (error) {
@@ -36,6 +36,7 @@ const processedPeople = persons.map((person) => {
     return {
       firstName: "",
       processedName: "",
+      fullName: ""
     };
   }
 });
