09061934772d96d7af82234765a289ca4c43d0c0
MarsGotBars <marcinm@live.nl>
2025-02-27 19:00:49 +0100

    script animatie voor de kaarten

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,2 +1,38 @@
+// Alle kaarten ophalen
+const cards = document.querySelectorAll(".card");
+
+// Hier bewaren we de kaarten die de view inkomen
+const initiallyVisible = [];
+
+// Observer opzetten
+const observer = new IntersectionObserver(
+  (entries, obs) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        initiallyVisible.push(entry.target);
+        obs.unobserve(entry.target);
       }
     });
+
+    if (initiallyVisible.length > 0) {
+      // Default delay van de animatie tussen kaarten
+      const delayBetweenCards = 75;
+      // Loop door de nieuw toegevoegde kaarten
+      initiallyVisible.forEach((card, index) => {
+        // Random nummer voor variatie
+        const randomMultiplier = Math.random() * (3 - 1) + 1;
+        // Timeout tussen class assignment
+        setTimeout(() => {
+          card.classList.add("bubble-up");
+        }, index * delayBetweenCards * randomMultiplier);
+      });
+      // Leegt de array
+      initiallyVisible.length = 0;
+    }
+  },
+  // Hoeveel % van de kaart moet visible zijn om de functie te triggeren
+  { threshold: 0.5 }
+);
+
+// Elke kaart observeren
+cards.forEach((card) => observer.observe(card));
